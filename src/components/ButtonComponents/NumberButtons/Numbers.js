4246b3d6e365a35165d3c694275040d55b091845
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-05 16:22:12 -0700

    added number button functionality

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,37 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {numbers} from '../../../data'
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
+
+
 //Import your array data to from the provided data file
 
+
 const Numbers = () => {
-  // STEP 2 - add the imported data to state
-  return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
-    </div>
-  );
+    // STEP 2 - add the imported data to state
+    const [numbersList] = useState(numbers)
+    // const createButtons = numbers.map((num)=> {
+    //     return (
+    //         <button key={num} onClick={() => console.log(num)}>{num}</button>
+    //     )
+    // })
+    // console.log(createButtons)
+    return (
+        numbersList.map((num) => {
+            if (num === '0') {
+                return (
+                    <button className='numButt zero' key={num}>{num}</button>
+                )
+            } else {
+                return (
+                    <button className='numButt ' key={num}>{num}</button>
+                )
+            }
+        })
+
+    )
+
+
 };
+export default Numbers
+
